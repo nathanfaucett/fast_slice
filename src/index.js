e45361eb9d53f7b8cc66e13d5864a6b67407650c
nathanfaucett <nathanfaucett@gmail.com>
2015-08-10 07:54:35 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
+var isNumber = require("is_number");
+
+
 module.exports = fastSlice;
 
 
 function fastSlice(array, offset) {
     var length, newLength, i, il, result, j;
 
-    offset = offset || 0;
+    offset = isNumber(offset) ? (offset <= 0 ? 0 : offset) : 0;
 
     length = array.length;
     i = offset - 1;
     il = length - 1;
     newLength = length - offset;
-    result = new Array(newLength <= 0 ? 0 : newLength);
+    result = new Array(newLength);
     j = 0;
 
     while (i++ < il) {
